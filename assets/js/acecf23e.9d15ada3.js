6650c4313b17231dc88fb90b51b4f330010a8b48
flurin-g <flurin-g@users.noreply.github.com>
2024-11-17 11:30:59 +0000

    Deploy updates 9a25a7438849af0c9efce2e30a95ff0242c8c23a

---
+++ assets/js/acecf23e.9d15ada3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_site=self.webpackChunkdocs_site||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/diskurs/blog","blogTitle":"Blog","authorsListPath":"/diskurs/blog/authors"}')}}]);
---
+++ assets/js/eb5f4cd3.e9cba039.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_site=self.webpackChunkdocs_site||[]).push([[7391],{7657:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/diskurs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
